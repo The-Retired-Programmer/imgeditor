1f53c9cf786674b4b0f44a8ea460c3794e266642
Richard Linsdale <richard@theretiredprogrammer.uk>
2019-06-15 21:16:03 +0100

    Add package level javadoc

---
+++ src/main/java/uk/theretiredprogrammer/imageditor/package-info.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019 richard linsdale.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ <p>IMG Editor (A NetBeans plugin)</p>
+ <p>The plugin provides a basic image editor for all image types recognised by NetBeans.</p>
+ <p>Image manipulations include:<br>
+ &nbsp;&nbsp;&nbsp;&nbsp;Resizing<br>
+ &nbsp;&nbsp;&nbsp;&nbsp;Cropping<br>
+ &nbsp;&nbsp;&nbsp;&nbsp;Multiple reduced sized image creation<br>
+ </p>
+ <p> ... more later ...</p>
+ */
+package uk.theretiredprogrammer.imageditor;
